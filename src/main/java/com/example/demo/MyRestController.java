91b41955d385fcd7eb5e416538ecb96a55033303
syr67 <anikin56789@mail.ru>
2023-09-22 21:21:43 +0300

    add

---
+++ src/main/java/com/example/demo/MyRestController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class MyRestController {
+
+    @RequestMapping(value = "/start", method = RequestMethod.POST)
+    public OutputObject myHandler(@RequestBody InputObject input) {
+
+        OutputObject output = new OutputObject();
+        output.setField1(input.getField1());
+        output.setResult("Request recived");
+        return output;
+
+
+    }
+
+}
---
+++ src/main/java/com/example/demo/OutputObject.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class OutputObject {
+    private String field1;
+    private String result;
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
